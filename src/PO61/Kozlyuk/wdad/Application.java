1b05974f98e65bc5196536629c05e4e299898a2a
MaximKozlyuk <maxim.kozlyuk@yandex.ru>
2018-09-20 01:06:08 +0400

    ПО-61 Козлюк Максим Валерьевич инициализацию успешно прошел. Готов приступить к выполнению следующего задания

--- src/PO61/Kozlyuk/wdad/Application.java
+++ src/PO61/Kozlyuk/wdad/Application.java
@@ -1,7 +1,9 @@
-public class Main {
+package PO61.Kozlyuk.wdad;
+
+public class Application {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("I’m Maxim Kozlyuk, and I’m not a monkey");
 
     }
 }
