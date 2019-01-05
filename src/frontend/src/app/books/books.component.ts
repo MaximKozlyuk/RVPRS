44434caab276b0d32294c924a730f21594396ac4
MaximKozlyuk <maxim.kozlyuk@yandex.ru>
2019-01-05 18:23:25 +0400

    about lib, sort by

--- src/frontend/src/app/books/books.component.ts
+++ src/frontend/src/app/books/books.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-books',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  private books : number[] = new Array<number>(4);
+  @Input() private books : number[] = new Array<number>(4);
 
   constructor() { }
 
